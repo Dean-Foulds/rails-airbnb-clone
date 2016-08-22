96f9e8a596dcc206174956d36f688c514221b1ce
sam cathcart <sc@10past12.co>
2016-08-22 17:47:05 +0100

    routes file added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :homes, do
+    resources :bookings, only: [:new, :create, :show]
+  end
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
