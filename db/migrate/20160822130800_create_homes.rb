b7c8a290dd00512036a929195899faaaacfaeba9
Dean Foulds <deanfoulds@gmail.com>
2016-08-22 18:10:10 +0100

    Fix migrations

--- db/migrate/20160822130800_create_homes.rb
+++ db/migrate/20160822130800_create_homes.rb
@@ -1,15 +1,15 @@
-class CreateProperties < ActiveRecord::Migration[5.0]
+class CreateHomes < ActiveRecord::Migration[5.0]
   def change
-    create_table :properties do |t|
-      t.references :user_id, foreign_key: true
+    create_table :homes do |t|
+      t.references :user, foreign_key: true
       t.string :address
       t.string :type
-      t.url :pictures
+      t.string :pictures
       t.integer :number_of_rooms
       t.integer :radius
       t.string :status
       t.string :area
-      t.text :comments
+      t.string :comments
 
       t.timestamps
     end
