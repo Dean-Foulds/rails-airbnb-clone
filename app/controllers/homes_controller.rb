74fa31245e4bf61a95bd3777e5fb37bb5c1afa93
sam cathcart <sc@10past12.co>
2016-08-23 16:16:32 +0100

    group edits to controller and views

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,38 +1,39 @@
 class HomesController < ApplicationController
-  # before_action :set_home, :only [:show]
+  before_action :set_home, only: [:show]
 
-    def index# GET /index
-      @homes = Home.all
+  def index# GET /index
+    @homes = Home.all
+  end
+
+  def show # GET /homes/by ID
+    @home = Home.find(params[:id])
+  end
+
+  def new  # GET /homes/new
+    @home = Home.new
+  end
+
+
+  def create # POST /homes
+    @home = Home.new(home_params)
+    @home.user = current_user
+    if @home.save
+      redirect_to home_path(@home)
+    else
+      render :new
     end
+  end
 
-    def show # GET /homes/by ID
-      @home = Home.new
-    end
-
-    def new  # GET /homes/new
-      @home = Home.new
-    end
-
-
-    def create # POST /homes
-      @home = Home.new(home_params)
-      if @home.save
-        redirect_to home_path(@home)
-      else
-        render :new
-      end
-    end
-
-    private
+  private
 
   # Never trust parameters from the scary
   # internet, only allow the white list.
 
   def set_home
-    @home = Home.find(params[:home_id])
+    @home = Home.find(params[:id])
   end
 
   def home_params
-    params.require(:home).permit(:xxxxx)
+    params.require(:home).permit(:address)
   end
 end
