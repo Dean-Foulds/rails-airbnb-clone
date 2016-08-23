dfd19179829bbbcbd5c13db6ec6656b62b1aed35
sam cathcart <sc@10past12.co>
2016-08-23 16:55:46 +0100

    added initial deop down lists

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,10 @@
 class Home < ApplicationRecord
   belongs_to :user
   has_attachments :photos, maximum: 3
+
+  STATUS = %w(pending, available, viewing, tenancy)
+
+  validates :name, presence: true
+  validates :status, inclusion: { in: STATUS }
+
 end
