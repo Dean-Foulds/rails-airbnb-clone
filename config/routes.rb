65ee78154da768ac77c8af6f98bb0a9979ff2180
sam cathcart <sc@10past12.co>
2016-08-23 10:38:05 +0100

    routes comma added for syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :homes, do
+  resources :homes do
     resources :bookings, only: [:new, :create, :show]
   end
 
   devise_for :users
+
   root to: 'pages#home'
 end
-
-
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
