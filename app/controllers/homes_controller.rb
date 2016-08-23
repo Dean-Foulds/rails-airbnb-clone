71175114df12ccd6374b9599b6dc473c058cac0b
sam cathcart <sc@10past12.co>
2016-08-23 14:58:55 +0100

    added new controllers

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,38 @@
+class HomesController < ApplicationController
+  # before_action :set_home, :only [:show]
+
+    def index# GET /index
+      @homes = Home.all
+    end
+
+    def show # GET /homes/by ID
+      @home = Home.new
+    end
+
+    def new  # GET /homes/new
+      @home = Home.new
+    end
+
+
+    def create # POST /homes
+      @home = Home.new(home_params)
+      if @home.save
+        redirect_to home_path(@home)
+      else
+        render :new
+      end
+    end
+
+    private
+
+  # Never trust parameters from the scary
+  # internet, only allow the white list.
+
+  def set_home
+    @home = Home.find(params[:home_id])
+  end
+
+  def home_params
+    params.require(:home).permit(:xxxxx)
+  end
+end
