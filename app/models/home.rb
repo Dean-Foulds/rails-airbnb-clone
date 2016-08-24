cf2df3eeb8889162b540d4b69736d5bf2af825c3
SaulGreenberg <sg@10past12.co>
2016-08-24 10:49:59 +0100

    changes made to index.html.erb file to fix error message, and also delete validations that referenced 'name' and added 'address'

--- app/models/home.rb
+++ app/models/home.rb
@@ -8,9 +8,9 @@ class Home < ApplicationRecord
   NUMBER_OF_ROOMS = %w(1 2 3 4)
   RADIUS = %w(1_mile 5_miles 10_miles 25_miles)
 
-  validates :name, presence: true
-  validates :status, inclusion: { in: STATUS }
-  validates :type, inclusion: { in: TYPE }
-  validates :number_of_rooms, inclusion: { in: NUMBER_OF_ROOMS }
-  validates :radius, inclusion: { in: RADIUS }
+  validates :address, presence: true
+  # validates :status, inclusion: { in: STATUS }
+  # validates :type, inclusion: { in: TYPE }
+  # validates :number_of_rooms, inclusion: { in: NUMBER_OF_ROOMS }
+  # validates :radius, inclusion: { in: RADIUS }
 end
