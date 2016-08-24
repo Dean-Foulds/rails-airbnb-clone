78f316c24afc4c5a5585ee9190887b35c0eb936f
sam cathcart <sc@10past12.co>
2016-08-24 12:15:52 +0100

    check css

--- app/models/home.rb
+++ app/models/home.rb
@@ -6,11 +6,10 @@ class Home < ApplicationRecord
   STATUS = %w(pending available appointments assigned)
   TYPE = %w(room studio flat maisonette bunglow house)
   NUMBER_OF_ROOMS = %w(1 2 3 4)
-  RADIUS = %w(1_mile 5_miles 10_miles 25_miles)
+  RADIUS = %w(1 5 10 25)
 
-  validates :name, presence: true
-  validates :status, inclusion: { in: STATUS }
-  validates :type, inclusion: { in: TYPE }
-  validates :number_of_rooms, inclusion: { in: NUMBER_OF_ROOMS }
-  validates :radius, inclusion: { in: RADIUS }
+  # validates :status, inclusion: { in: STATUS }
+  # validates :type, inclusion: { in: TYPE }
+  # validates :number_of_rooms, inclusion: { in: NUMBER_OF_ROOMS }
+  # validates :radius, inclusion: { in: RADIUS }
 end
