8a99c2bc2b12753ec3979ff79f950a4baf190c6d
Dean Foulds <deanfoulds@gmail.com>
2016-08-29 19:27:12 +0100

    email link modifed to work on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # after_create :send_welcome_email
+  after_create :send_welcome_email
 
-  # private
+  private
 
-  # def send_welcome_email
-  #   UserMailer.welcome(self).deliver_now
-  # end
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 end
