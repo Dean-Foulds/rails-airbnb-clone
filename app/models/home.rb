6e4d91aa2a7a2216d1e1eae0e3ccd038bedbe58b
Dean Foulds <deanfoulds@gmail.com>
2016-08-30 14:51:39 +0100

    change back to old seed

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,7 +1,7 @@
 class Home < ApplicationRecord
   has_many :bookings
   belongs_to :user
-  has_attachments :pictures, maximum: 3
+  has_attachments :pictures, maximum: 10
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
