3560254a52728cb480da309700f3ab738c62a800
SaulGreenberg <sg@10past12.co>
2016-08-30 14:52:32 +0100

    changes to bookings controller and bookings view, deleting bookings show and adding bookings index, also iteration within iteration to create list in index per home and booking

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :bookings, only: :show
 
   resources :homes do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:index, :new, :create]
   end
 
   devise_for :users
