287c6b0264171771ac04033e272c757c41c82bb4
sam cathcart <sc@10past12.co>
2016-09-02 00:15:35 +0100

    panels adjusted

--- db/schema.rb
+++ db/schema.rb
@@ -87,26 +87,26 @@ ActiveRecord::Schema.define(version: 20160901162742) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "company"
     t.string   "address"
     t.string   "city"
     t.string   "post_code"
     t.string   "mobile_number"
-    t.boolean  "admin"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
