8ca76428c1e2f6a7e72b413519ac5c54461e16b8
deanfoulds <deanfoulds@gmail.com>
2016-09-04 20:07:34 +0100

    form update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,24 @@
 class MessagesController < ApplicationController
-def show
-    authorize current_user
-end
-
 def new
-  @message = Message.new
-  authorize @message
-end
-
-def edit
-  authorize current_user
-end
-
-def update
-  authorize current_user
-  if current_user.update(mesage_params)
-    redirect_to edit_profile_path
-  else
-    render :edit
+    @message = Message.new
   end
-end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.valid?
+      MessageMailer.new_message(@message).deliver
+      redirect_to contact_path, notice: "Your messages has been sent."
+    else
+      flash[:alert] = "An error occurred while delivering this message."
+      render :new
+    end
+  end
 
 private
 
-def message_params
-  params.require(:message).permit(:name, :email, :content)
-end
+  def message_params
+    params.require(:message).permit(:name, :email, :content)
+  end
 
 end
