d7ec34540bd56bb80408a5d186356a9d25e24868
deanfoulds <deanfoulds@gmail.com>
2016-09-05 17:45:08 +0100

    landlord properties added to profile

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
-  skip_after_action :verify_authorized
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized, except: :index
+  skip_after_action :verify_policy_scoped, only: :index
 def new
     @message = Message.new
   end
