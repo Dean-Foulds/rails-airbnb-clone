7b84c9aa1d25a0a6d7140fa2aef419bb410aa515
Dean Foulds <deanfoulds@gmail.com>
2016-09-07 18:39:31 +0100

    minimum content on home review model changed to 1

--- app/models/home_review.rb
+++ app/models/home_review.rb
@@ -2,7 +2,7 @@ class HomeReview < ApplicationRecord
   belongs_to :user
   belongs_to :home
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 1}
   validates :rating, presence: true
   validates :rating, inclusion: {in: (1..5).to_a}
   validates :home, presence: true
