13501cf0f7bd89ff8c5bd9f106ac701c3830447f
Dean Foulds <deanfoulds@gmail.com>
2016-09-08 09:03:48 +0100

    character length line seperated

--- app/models/home_review.rb
+++ app/models/home_review.rb
@@ -2,7 +2,8 @@ class HomeReview < ApplicationRecord
   belongs_to :user
   belongs_to :home
 
-  validates :content, presence: true, length: { minimum: 1}
+  validates :content, presence: true
+  validates :content, length: { minimum: 1}
   validates :rating, presence: true
   validates :rating, inclusion: {in: (1..5).to_a}
   validates :home, presence: true
